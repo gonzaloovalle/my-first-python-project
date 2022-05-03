2bf3969e7fe625651253a94a71239d030d9e3813
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-05-03 15:44:17 -0500

    first half of python 1-hr tutorial

---
+++ app.py
@@ -0,0 +1,40 @@
+###Variable Types###
+# age = 20
+# price = 19.95
+# first_name = "Gonzalo"
+# is_online = True
+# print(age)
+
+###Input Method###
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+###Type Coercion###
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print("Your age is " + str(age))
+
+# input1 = input("First: ")
+# num1 = float(input1)
+# input2 = input("Second: ")
+# num2 = float(input2)
+# sum = float(num1 + num2)
+# print("Sum: " + str(sum))
+
+##Alternate Solution##
+# first = float(input("First: "))
+# second = float(input("Second: "))
+# sum = first + second
+# print("Sum: " + str(sum))
+
+###Strings###
+# course = "Python for Beginners"
+# print(course.find("y"))
+# print(course.find("Y"))
+# print(course.find("for"))
+# print(course.replace("for", "4"))
+# print(course.find("Python"))
+# print("Python" in course)
+
+###Arithmetic Operators###
+print(10 + 3)
